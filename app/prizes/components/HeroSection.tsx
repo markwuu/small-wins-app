d4eb4bef434efcf1c1294a4fbde8ce749f23bc9d
Mark W <markwuu@gmail.com>
2025-10-24 03:09:33 -0700

    renamed index

--- app/prizes/components/HeroSection.tsx
+++
@@ -1,3 +0,0 @@
-export default function HeroSection() {
-  return <h1>Hero Section</h1>;
-}
---
+++ app/prizes/feature/HeroSection.tsx
@@ -0,0 +1,8 @@
+export default function HeroSection() {
+  return (
+    <div>
+      <h1>Hero Section</h1>
+      <button>Add Prize</button>
+    </div>
+  );
+}
